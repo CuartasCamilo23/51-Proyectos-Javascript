c20fea8ac5f2d77d4ebd1ec7bab69902303b77c1
Camilo Cuartas <juancamilocuartas321@gmail.com>
2026-08-19 01:11:55 -0500

    contador funcional v1

--- 00-Contador.js/script.js
+++ 00-Contador.js/script.js
@@ -1,3 +1,19 @@
 const numero = document.querySelector(".main_number");
 
-console.log(numero)
+const botonPlus = document.querySelector(".plus-button")
+const botonLess = document.querySelector(".less-button")
+const botonReset = document.querySelector(".reset-button")
+
+function sumar() {
+    numero.textContent =  Number(numero.textContent)+1;
+}
+function resta() {
+    numero.textContent = Number(numero.textContent) -1;
+}
+function resetear(){
+    numero.textContent = 0;
+}
+
+botonPlus.addEventListener("click", sumar);
+botonLess.addEventListener("click", resta)
+botonReset.addEventListener("click", resetear)
