189f4620d9cb39e19206fe6d1e817269023cb6a5
Camilo Cuartas <juancamilocuartas321@gmail.com>
2026-08-19 15:34:36 -0500

    Inicios JavaScript

---
+++ 01-Generador de Colores/script.js
@@ -0,0 +1,19 @@
+const colorCode = document.querySelector(".color-code")
+
+const containerColor = document.querySelector(".container-color")
+
+const buttonChange = document.querySelector(".button-change")
+
+const caracteres = "0123456789ABCDEF";
+
+function concatenacion (){
+   return caracteres[Math.floor(Math.random() * 16)] + caracteres[Math.floor(Math.random() * 16)] + caracteres[Math.floor(Math.random() * 16)] + caracteres[Math.floor(Math.random() * 16)] + caracteres[Math.floor(Math.random() * 16)] + caracteres[Math.floor(Math.random() * 16)];
+}
+
+let resultado = "";
+
+fo
+{
+
+}
+console.log(concatenacion())
