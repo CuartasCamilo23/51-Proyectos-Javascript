f184c066525b3653ecb77492cda4dff6c954a932
Camilo Cuartas <juancamilocuartas321@gmail.com>
2026-08-19 23:43:55 -0500

    Terminado nuevamente

--- 01-Generador de Colores/script.js
+++ 01-Generador de Colores/script.js
@@ -1,37 +1,33 @@
 const colorCode = document.querySelector(".color-code")
 
-const containerColor = document.querySelector(".container-color")
+const boxColor = document.querySelector(".container-color")
 
 const buttonChange = document.querySelector(".button-change")
 
 const caracteres = "0123456789ABCDEF";
 
-let resultado = "";
 
-function concatenacion() {
+function concatenacion(){
+   
+   let resultado ="";
 
-   let resultado = "";
+   for (let i=0; i < 6; i++ ){
 
-   for (let i = 0; i < 6; i++) {
-        resultado = resultado + caracteres[Math.floor(Math.random() * 16)];
+      resultado = resultado + caracteres[Math.floor(Math.random() * 16)]
+
    }
-
+   
    return resultado;
 }
 
-const color = "#" + concatenacion();
 
-containerColor.style.backgroundColor =color;
 
-function cambiarColor() {
 
+
+function cambioColor (){
    const color = "#" + concatenacion();
-
-   containerColor.style.backgroundColor = color;
-
+   boxColor.style.backgroundColor =color;
    colorCode.textContent = color;
-
 }
 
-buttonChange.addEventListener("click", cambiarColor);
-
+buttonChange.addEventListener("click", cambioColor)
