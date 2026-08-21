0a1b97372f2439020a85d89b48bf870077bbeb7c
Camilo Cuartas <juancamilocuartas321@gmail.com>
2026-08-20 23:51:36 -0500

    Proyecto 04 Terminado

--- 04-Generados de Contraseñas/script.js
+++ 04-Generados de Contraseñas/script.js
@@ -2,3 +2,21 @@ const contraseña = document.querySelector(".password")
 
 const button = document.querySelector(".button")
 
+const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+function generarContraseña() {
+
+    let valores = "";
+
+        for (let i = 0; i < 8; i++ ){
+            valores = valores + caracteres[Math.floor(Math.random() * caracteres.length)]
+        }
+        return valores
+}
+
+function cambioContraseña(){
+    contraseña.textContent = generarContraseña()
+}
+
+
+button.addEventListener("click", cambioContraseña)
