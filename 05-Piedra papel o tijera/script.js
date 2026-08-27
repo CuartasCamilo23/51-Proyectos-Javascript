e33469960d7ed0382a9793b202b88ef460e25585
Camilo Cuartas <juancamilocuartas321@gmail.com>
2026-08-27 10:53:25 -0500

    inicio JavaScript

---
+++ 05-Piedra papel o tijera/script.js
@@ -0,0 +1,4 @@
+const botones = document.querySelectorAll(".button")
+const piedra = document.querySelector(".piedra")
+const papel = document.querySelector(".papel")
+const tijera = document.querySelector(".tijera")
